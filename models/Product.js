d4fe4ee22f5b4f05672e1ca7ae83b399cb5c50ff
Ian Daluz <janiandaluz@yahoo.com>
2024-07-14 03:24:18 -0600

    setting up fields and rules for product model

--- models/Product.js
+++ models/Product.js
@@ -9,14 +9,49 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(),
+      allowNull: false,
+      validate: {
+        isDecimal: {
+          msg: "Price must be a decimal number.",
+        },
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: {
+          msg: "Stock must be a numeric value.",
+        },
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "products",
   }
 );
 
