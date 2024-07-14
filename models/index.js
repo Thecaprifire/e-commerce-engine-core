d0cabb64b92e90a8622b452b85a501fa92ed22d0
Ian Daluz <janiandaluz@yahoo.com>
2024-07-14 03:38:03 -0600

    added code to index.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,38 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  // alias for the association
+  as: "product_name",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  unique: false,
+  // alias for the association
+  as: "tags",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+  // alias for the association
+  as: "products",
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  // alias for the association
+  as: "products",
+});
 
 module.exports = {
   Product,
