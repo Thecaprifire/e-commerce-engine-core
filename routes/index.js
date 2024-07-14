44fab31f4e78a2953e68dc5eab5d1ac9186a8d71
Ian Daluz <janiandaluz@yahoo.com>
2024-07-14 03:39:14 -0600

    added code to route/api

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
+
+router.get("/", (req, res) => {
+  res.send("<h1>Hello, this is the root route!<h1>");
+});
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  res.send("<h1>Wrong Route! Testing</h1>");
 });
 
 module.exports = router;
